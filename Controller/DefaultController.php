9b5611370dac18a0fbd5714cbc88b178e9570864
PurpleBabar <lalung.alexandre@gmail.com>
2017-03-16 19:16:58 +0100

    ending with need to add a hydrator

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,13 +3,25 @@
 namespace Elephantly\OGBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Elephantly\OGBundle\Entity\OGObject;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         $crawler = $this->get('elephantly.og.crawler');
-        \Doctrine\Common\Util\Debug::dump($crawler->getHead('http://opengraphprotocol.org'));exit;
+        $ogs = $crawler->getHead('http://opengraphprotocol.org');
+        $og = new OGObject();
+        $og->setTitle($ogs['title']);
+        $og->setType($ogs['type']);
+        $og->setUrl($ogs['url']);
+        $og->setImage($ogs['image']);
+        $og->setDescription($ogs['description']);
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($og);
+        $em->flush();
+
         return $this->render('ElephantlyOGBundle:Default:index.html.twig');
     }
 }
